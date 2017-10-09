53127712423c5c59f76bd71a0f0c8f557383836c
Park Kyung Won <park0kyung0won@gmail.com>
2017-10-09 14:25:08 +0000

    dsadasd

--- oddeven/do_hw_batch.sh
+++ oddeven/do_hw_batch.sh
@@ -1,17 +1,17 @@
 make
 mpirun -n 1 ./oddeven 2
-mpirun -n 1 ./oddeven 4
-mpirun -n 1 ./oddeven 8
-mpirun -n 1 ./oddeven 16
 mpirun -n 2 ./oddeven 2
-mpirun -n 2 ./oddeven 4
-mpirun -n 2 ./oddeven 8
-mpirun -n 2 ./oddeven 16
 mpirun -n 4 ./oddeven 2
-mpirun -n 4 ./oddeven 4
-mpirun -n 4 ./oddeven 8
-mpirun -n 4 ./oddeven 16
 mpirun -n 8 ./oddeven 2
+mpirun -n 1 ./oddeven 4
+mpirun -n 2 ./oddeven 4
+mpirun -n 4 ./oddeven 4
 mpirun -n 8 ./oddeven 4
+mpirun -n 1 ./oddeven 8
+mpirun -n 2 ./oddeven 8
+mpirun -n 4 ./oddeven 8
 mpirun -n 8 ./oddeven 8
+mpirun -n 1 ./oddeven 16
+mpirun -n 2 ./oddeven 16
+mpirun -n 4 ./oddeven 16
 mpirun -n 8 ./oddeven 16
